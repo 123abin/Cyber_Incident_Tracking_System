145306a18cf67289299455ec46dc345bcaae492a
Abin sebastian <7abinsebastian@gmail.com>
2026-04-03 20:24:57 +0100

    Completed full authentication flow(register, login, validation,dashboard)

--- setup_db.py
+++ setup_db.py
@@ -4,7 +4,7 @@ conn = sqlite3.connect("database.db")
 cursor = conn.cursor()
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT,
-               username TEXT NOT NULL,password TEXT NOT NULL)""")
+               username TEXT  NOT NULL,password TEXT NOT NULL)""")
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS incidents (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT,
                type TEXT,severity TEXT,description TEXT,status TEXT)""")
